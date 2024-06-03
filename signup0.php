33e223c2d80be33d11a7e856c00d48307667f0c0
sykrmzffr <amazeme00@gmail.com>
2024-06-03 20:51:32 +0800

    update signup0.php for chosen_adm

--- signup0.php
+++ signup0.php
@@ -8,11 +8,24 @@ session_start();
 	$user_fname = $_REQUEST['fname'];
 	$user_lname = $_REQUEST['lname'];
 	$user_password = $_REQUEST['password'];
+	$chosen_adm = $_REQUEST['chosen_adm'];
 	
-	
+	if ($chosen_adm == 'zarith') {
+		$chosen_adm='zarith@gmail.com';
+	}
+	else if ($chosen_adm == 'auni') {
+		$chosen_adm='auni@gmail.com';
+	}
+	else if ($chosen_adm == 'faris') {
+		$chosen_adm='faris@gmail.com';
+	}
+	else if ($chosen_adm == 'syakir') {
+		$chosen_adm='syakir@gmail.com';
+	}
+
 	$sqlInsert = "INSERT INTO user VALUES
 	('" . $user_email . "','" . $user_fname . "',
-	 '" . $user_lname . "','" . $user_password . "')";
+	 '" . $user_lname . "','" . $user_password . "', '" . $chosen_adm . "')";
 	 
 	 mysqli_query($dbconn, $sqlInsert) or die ("Error: " . mysqli_error($dbconn));
 	 echo "The following information have been recorded in the DB";
